9cf45d009909b903e3819a77bd34355823342ee6
CalebColby <ccolby@student.neumont.edu>
2023-08-16 17:26:56 -0600

    added LAN Script

---
+++ card against humanity Project/LAN Muliplayer/LANMuliplayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace card_against_humanity_Project.LAN_Muliplayer
+{
+    public class LANMuliplayerHost
+    {
+
+        public IPAddress hostadresss { get; private set; }
+        private List<IPAddress> guests { get; set; }
+    }
+
+    public class LANMultiplayerGuest
+    {
+        public IPAddress me { get; private set; }
+    }
+}
