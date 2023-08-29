3d20d08554539ff1458370ff3510398ae347095c
CalebColby <ccolby@student.neumont.edu>
2023-08-29 15:27:56 -0600

    commit_now

--- card against humanity Project/LAN Muliplayer/TcpServer.cs
+++ card against humanity Project/LAN Muliplayer/TcpServer.cs
@@ -1,45 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace card_against_humanity_Project.LAN_Muliplayer
 {
     internal class TcpServer
     { 
-        private TcpListener _listener;
+        private TcpListener server = null;
+        private List<TcpClient> clients = new List<TcpClient>();
+        private CancellationTokenSource cancellationService = new CancellationTokenSource();
         public TcpServer() 
         {
-            StartServer();
+            _ = StartServer();
+        }
+
+        ~TcpServer()
+        {
+            if(server != null) server.Stop();
         }
 
         private void StartServer()
         {
-            var port = 13000;
-            var hostAddress = IPAddress.Parse("127.0.0.1");
-            _listener = new TcpListener(hostAddress, port);
-            _listener.Start();
-
-            byte[] buffer = new byte[256];
-            string recievedMessage;
-
-            TcpClient clinet = _listener.AcceptTcpClient();
-
-            var tcpStream = clinet.GetStream();
-
-            int readTotal;
-
-            while((readTotal = tcpStream.Read(buffer, 0, buffer.Length)) != 0)
+            try
             {
-                string incomingMessage = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+                var port = 1998;
+                var hostAddress = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(hostAddress, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                _ = AcceptClients();
+                _ = HandleTcpClinets();
             }
-            clinet.Close();
+            catch (SocketException e)
+            {
+                server?.Stop();
+                Console.WriteLine("SocketException: {0}", e);
+            }
         }
 
+        private async Task AcceptClients()
+        {
+            while(!cancellationService.IsCancellationRequested)
+            {
+                var client = server.AcceptTcpClient();
 
+                clients.Add(client);
+            }
+        }
+        private async Task HandleTcpClinets()
+        {
+            while (!cancellationService.IsCancellationRequested)
+            {
+                if (clients.Count == 0) continue;
 
+                foreach (var client in clients)
+                {
+
+                }
+            }
+        }
     }
 }
