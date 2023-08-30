80f609fe6d8175a716ca45d303d2d9bbdfa3a2be
CalebColby <ccolby@student.neumont.edu>
2023-08-30 00:53:59 -0600

    added some Game Functionality

--- CAHP_ASP/Controllers/GameController.cs
+++ CAHP_ASP/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CAHP_ASP.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace CAHP_ASP.Controllers
@@ -6,13 +7,29 @@ namespace CAHP_ASP.Controllers
     public class GameController : Controller
     {
         public bool IsHost { private set; get; } = false;
-
+        public GameData _gameData { private set; get; } = new GameData();
 
         public IActionResult Lobby()
         {
-            var ipAddress = Dns.GetHostName();
+            _gameData.Players.Add(new Player("Host"));
+            IsHost = true;
 
-            return View();
+            return View(_gameData);
+        }
+
+        [HttpPost("JoinLobby")]
+        public IActionResult JoinLobby(Player player)
+        {
+            _gameData.Players.Add(player);
+
+            return View(_gameData);
+        }
+
+        public IActionResult GameAction()
+        {
+
+
+            throw new NotImplementedException();
         }
     }
 }
--- CAHP_ASP/Models/GameData.cs
+++ CAHP_ASP/Models/GameData.cs
@@ -3,5 +3,6 @@
     public class GameData
     {
         public int Id { get; set; }
+        public List<Player> Players { get; set; } = new List<Player>();
     }
 }
